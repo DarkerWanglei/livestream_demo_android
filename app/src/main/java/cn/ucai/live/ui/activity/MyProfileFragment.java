a468054bf943b7738e1186be36ce8c0fb7095348
wanglei <912436316@qq.com>
2017-02-28 11:46:26 +0800

    个人中心头像昵称显示以及退出登录方法

--- app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
+++ app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
@@ -7,16 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.ucai.live.LiveHelper;
 import cn.ucai.live.data.model.LiveSettings;
 
 import cn.ucai.live.R;
+import cn.ucai.live.utils.MFGT;
+
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 
 public class MyProfileFragment extends Fragment {
     Unbinder unbinder;
@@ -24,8 +31,10 @@ public class MyProfileFragment extends Fragment {
     //Spinner spinner;
     //@BindView(R.id.frame_rate)
     //TextView frameRateText;
-    @BindView(R.id.tv_username) TextView usernameView;
-
+    @BindView(R.id.tv_username)
+    TextView usernameView;
+    @BindView(R.id.iv_avatar)
+    ImageView ivAvatar;
     LiveSettings liveSettings;
 
     @Nullable
@@ -40,8 +49,9 @@ public class MyProfileFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        usernameView.setText(EMClient.getInstance().getCurrentUser());
-
+//        usernameView.setText(EMClient.getInstance().getCurrentUser());
+        EaseUserUtils.setAppUserAvatarByPath(getContext(), EMClient.getInstance().getCurrentUser(), ivAvatar);
+        EaseUserUtils.setAppUserNick(EMClient.getInstance().getCurrentUser(), usernameView);
 
         //liveSettings = new LiveSettings(getContext());
         //final String[] bitrateArr = getResources().getStringArray(R.array.bitrate_types);
@@ -65,17 +75,18 @@ public class MyProfileFragment extends Fragment {
 
     }
 
-    @OnClick(R.id.btn_logout) void onLogout(){
-        EMClient.getInstance().logout(false, new EMCallBack() {
+    @OnClick(R.id.btn_logout)
+    void onLogout() {
+        LiveHelper.getInstance().logout(false, new EMCallBack() {
             @Override
             public void onSuccess() {
                 getActivity().finish();
-                startActivity(new Intent(getActivity(), LoginActivity.class));
+                MFGT.gotoLoginCleanTask(getActivity());
             }
 
             @Override
             public void onError(int i, String s) {
-
+                Toast.makeText(getActivity(), "unbind devicetokens failed", Toast.LENGTH_SHORT).show();
             }
 
             @Override
